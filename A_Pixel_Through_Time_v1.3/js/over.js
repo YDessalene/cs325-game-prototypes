6e1b77d54d52fb79d9d503e7f5bd4c269fd3b158
YDessalene <37000615+YDessalene@users.noreply.github.com>
2019-04-18 14:45:09 -0400

    Update over.js

--- A_Pixel_Through_Time_v1.3/js/over.js
+++ A_Pixel_Through_Time_v1.3/js/over.js
@@ -2,6 +2,7 @@ var gameOver = function(game) {};
 
 gameOver.prototype = {
 	create: function() {
+		music.stop();
 		this.game.add.text(200, 200, 'To Be Continued...\n\nPress R to restart scenes', { fontSize: '32px', fill: '#ffffff' });
 		this.game.input.keyboard.addKey(Phaser.Keyboard.R).onDown.addOnce(this.restart, this);
 	},
